c05435b1de07551bf7f4a6ec8fcc3b4715cd0a64
Alex Palaversich <palaalex1@gmail.com>
2024-09-08 09:05:22 -0300

    feat auth without ws

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var cors = require('cors')
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
 const app = express();
 app.use(cors())
@@ -8,63 +8,92 @@ app.use(express.json());
 
 
 const PORT = 3002;
-const wss = new WebSocket.Server({ port: 3003 });
+// const wss = new WebSocket.Server({ port: 3003 });
 
 const clients = new Map();
 const DISCONNECT_TIMEOUT = 40000;
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
 
-  let authCode = Date.now().toString(36) + Math.random().toString(36).substring(13);
+//   let authCode = Date.now().toString(36) + Math.random().toString(36).substring(13);
 
-  clients.set(authCode, ws);
-  ws.send(JSON.stringify({ status: 'authCode', authCode: authCode }));
+//   clients.set(authCode, ws);
+//   ws.send(JSON.stringify({ status: 'authCode', authCode: authCode }));
 
-  const disconnectTimeout = setTimeout(() => {
-    if (clients.has(authCode)) {
-      clients.delete(authCode);
-      console.log('Disconnecting client due to inactivity:', authCode);
-    }
-    ws.close();
-  }, DISCONNECT_TIMEOUT);
+//   const disconnectTimeout = setTimeout(() => {
+//     if (clients.has(authCode)) {
+//       clients.delete(authCode);
+//       console.log('Disconnecting client due to inactivity:', authCode);
+//     }
+//     ws.close();
+//   }, DISCONNECT_TIMEOUT);
 
-  ws.on('close', () => {
-    console.log('Client disconnected');
-    clients.delete(authCode);
-  });
+//   ws.on('close', () => {
+//     console.log('Client disconnected');
+//     clients.delete(authCode);
+//   });
+// });
+
+// app.post('/authCode/:authCode', (req, res) => {
+//   const { authCode } = req.params;
+
+//   console.log(req.body)
+
+//   const client = clients.get(authCode);
+
+//   if (client) {
+//     console.log(`Auth code verified: ${authCode} , user: ${req.body.username}`);
+//     client.send(JSON.stringify({ status: 'verified', message: 'QR code scanned successfully!' }));
+
+//     client.close();
+//     clients.delete(authCode);
+
+//     res.status(200).send('Auth code verified!');
+//   } else {
+//     console.log('Auth code did not match.');
+//     res.status(400).send('Invalid or expired auth code.');
+//   }
+// })
+
+app.get('/authCode/', (req, res) => {
+  let authCode = Date.now().toString(36) + Math.random().toString(36).substring(2);
+
+  let newClient = { authCode: authCode, verified: false };
+
+  clients.set(authCode, newClient);
+
+  res.status(200).json(newClient);
 });
 
-app.post('/authCode/:authCode', (req, res) => {
-  const { authCode } = req.params;
 
-  console.log(req.body)
+app.get('/authCodeCheck/:authCode', (req, res) => {
+  let client = clients.get(req.params.authCode);
+  console.log(client.authCode + " checking")
+  res.status(200).json(client);
+})
 
-  const client = clients.get(authCode);
+app.get('/authCode/:authCode', (req, res) => {
+  let client = clients.get(req.params.authCode);
+  console.log(req.params.authCode + " recived")
 
   if (client) {
-    console.log(`Auth code verified: ${authCode} , user: ${req.body.username}`);
-    client.send(JSON.stringify({ status: 'verified', message: 'QR code scanned successfully!' }));
-
-    client.close();
-    clients.delete(authCode);
-
-    res.status(200).send('Auth code verified!');
-  } else {
-    console.log('Auth code did not match.');
-    res.status(400).send('Invalid or expired auth code.');
+    client.verified = true;
+    console.log(`Auth code verified: ${req.params.authCode}`);
   }
-})
+
+  res.status(200).json(client);
+});
 
 app.post("/uploadFile", async (req, res) => {
   const { storageRef } = req.body;
   const { file } = req.body;
-  try{
+  try {
     await uploadBytes(storageRef, file).then((snapshot) => {
       console.log("Uploaded a blob or file!");
       res.status(200).send('File uploaded successfully!');
     })
-  } catch (err){
+  } catch (err) {
     console.log(err)
     res.status(500).send(err)
   }
