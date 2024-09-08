a9cb21c27c70f23a0a8d69189c8fa420ccbde029
Alex Palaversich <palaalex1@gmail.com>
2024-09-08 01:40:54 -0300

    feat timeout

--- index.js
+++ index.js
@@ -2,25 +2,16 @@ const express = require('express');
 var cors = require('cors')
 const WebSocket = require('ws');
 
-// const admin = require('firebase-admin');
-// const serviceAccount = require('./firebase.json');
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-//   });
-
-//   const db = admin.database();
-
 const app = express();
 app.use(cors())
+app.use(express.json());
+
 
 const PORT = 3002;
 const wss = new WebSocket.Server({ port: 3003 });
 
 const clients = new Map();
-const DISCONNECT_TIMEOUT = 60000; // 1 minute in milliseconds
-
-
+const DISCONNECT_TIMEOUT = 40000;
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
@@ -28,13 +19,15 @@ wss.on('connection', (ws) => {
   let authCode = Date.now().toString(36) + Math.random().toString(36).substring(13);
 
   clients.set(authCode, ws);
-  ws.send(JSON.stringify({status: 'authCode', authCode: authCode }));
+  ws.send(JSON.stringify({ status: 'authCode', authCode: authCode }));
 
   const disconnectTimeout = setTimeout(() => {
-    console.log('Disconnecting client due to inactivity:', authCode);
-    ws.close(); // Close the WebSocket connection
-    clients.delete(authCode); // Remove the client from the map
-}, DISCONNECT_TIMEOUT);
+    if (clients.has(authCode)) {
+      clients.delete(authCode);
+      console.log('Disconnecting client due to inactivity:', authCode);
+    }
+    ws.close();
+  }, DISCONNECT_TIMEOUT);
 
   ws.on('close', () => {
     console.log('Client disconnected');
@@ -42,15 +35,18 @@ wss.on('connection', (ws) => {
   });
 });
 
-app.get('/authCode/:authCode', (req, res) => {
+app.post('/authCode/:authCode', (req, res) => {
   const { authCode } = req.params;
+
+  console.log(req.body)
 
   const client = clients.get(authCode);
 
   if (client) {
-    console.log("Auth code matched: ", authCode);
+    console.log(`Auth code verified: ${authCode} , user: ${req.body.username}`);
     client.send(JSON.stringify({ status: 'verified', message: 'QR code scanned successfully!' }));
 
+    client.close();
     clients.delete(authCode);
 
     res.status(200).send('Auth code verified!');
@@ -58,6 +54,21 @@ app.get('/authCode/:authCode', (req, res) => {
     console.log('Auth code did not match.');
     res.status(400).send('Invalid or expired auth code.');
   }
+})
+
+app.post("/uploadFile", async (req, res) => {
+  const { storageRef } = req.body;
+  const { file } = req.body;
+  try{
+    await uploadBytes(storageRef, file).then((snapshot) => {
+      console.log("Uploaded a blob or file!");
+      res.status(200).send('File uploaded successfully!');
+    })
+  } catch (err){
+    console.log(err)
+    res.status(500).send(err)
+  }
+  return res;
 })
 
 app.get('/', (req, res) => {
